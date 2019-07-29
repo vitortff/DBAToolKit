92e5ad82cacb3d219183dd49e74dd7121a05b2a6
Vitor Fava <vitortff@gmail.com>
2019-07-29 15:28:01 -0300

    Create CheckAllDatabaseSize.sql

---
+++ CheckAllDatabaseSize.sql
@@ -0,0 +1,29 @@
+SELECT [Database Name] = DB_NAME(database_id),
+
+       [Type] = CASE WHEN Type_Desc = 'ROWS' THEN 'Data File(s)'
+
+                     WHEN Type_Desc = 'LOG'  THEN 'Log File(s)'
+
+                     ELSE Type_Desc END,
+
+       [Size in MB] = CAST( ((SUM(Size)* 8) / 1024.0) AS DECIMAL(18,2) )
+
+FROM   sys.master_files
+
+— Uncomment if you need to query for a particular database
+
+— WHERE      database_id = DB_ID(‘Database Name’)
+
+GROUP BY      GROUPING SETS
+
+              (
+
+                     (DB_NAME(database_id), Type_Desc),
+
+                     (DB_NAME(database_id))
+
+              )
+
+ORDER BY      DB_NAME(database_id), Type_Desc DESC
+
+GO
