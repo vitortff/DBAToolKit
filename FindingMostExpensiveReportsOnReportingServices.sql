c206939d7fa6dab9a92ff426612b7f8ab612a35e
Vitor Fava <vitortff@gmail.com>
2019-11-20 18:34:00 -0300

    Create FindingMostExpensiveReportsOnReportingServices.sql
    Encontrar reports mais pesados no SSRS

---
+++ FindingMostExpensiveReportsOnReportingServices.sql
@@ -0,0 +1,4 @@
+--https://blogs.msdn.microsoft.com/robertbruckner/2009/01/06/executionlog2-view-analyzing-and-optimizing-reports/
+
+use ReportServer
+select * from ExecutionLog2 order by TimeStart DESC
